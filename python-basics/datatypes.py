c27a3490eaf6e5a6504b6472d88a2a230bb2dab6
denizzakirov0-star <denizzakirov0@gmail.com>
2026-01-29 21:36:17 +0500

    python programs

---
+++ python-basics/datatypes.py
@@ -0,0 +1,6 @@
+integer_number = 10      # int
+decimal_number = 3.14    # float
+is_true = True           # bool
+print(type(integer_number))
+print(type(decimal_number))
+print(type(is_true))
---
+++ python-basics/strings.py
@@ -0,0 +1,5 @@
+greeting = "Hello"
+name = "Bob"
+message = greeting + " " + name  # объединение строк
+print(message)
+print(f"Привет, {name}!")        # форматирование строк
